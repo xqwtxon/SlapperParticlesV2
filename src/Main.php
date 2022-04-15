e3a7f38279c7108df5265aa3bd5774cda723b7c8
junjun2527 <xqwtxon@hotmail.com>
2022-04-15 20:54:24 +0800

    Fuck u poggit

--- src/Main.php
+++ src/Main.php
@@ -14,7 +14,7 @@ class Main extends PluginBase implements SlapperParticleInfo {
         $this->saveResource("config.yml");
         $log = $this->getServer()->getLogger();
         $config = $this->getConfig();
-        if ($config->get("config-version") == SlapperParticlesInfo::CONFIG_VERSION){
+        if ($config->get("config-version") == SlapperParticleInfo::CONFIG_VERSION){
             @rename($this->getDataFolder()."/"."config.yml", $this->getDataFolder()."/"."old-config.yml");
             $log->notice("[NOTICE] Your configuration is outdated! The configuration was renamed as old-config.yml");
             $this->saveResource("config.yml");
@@ -22,7 +22,7 @@ class Main extends PluginBase implements SlapperParticleInfo {
             $this->saveDefaultConfig();
             $log->info("[INFO] The plugin was loaded!");
         }
-        if (SlapInfo::IS_DEVELOPMENT_BUILD == true){
+        if (SlapperParticleInfo::IS_DEVELOPMENT_BUILD == true){
             $log->warning(TextFormat::RED."Your SlapperParticle is in development build! You may expect crash during the plugin. You can make issue about this plugin by visiting plugin github issue!");
             return;
         }
@@ -31,25 +31,25 @@ class Main extends PluginBase implements SlapperParticleInfo {
         $config = $this->getConfig();
         $log = $this->getServer()->getLogger();
         $toggle = $config->get("enabled");
-            if (SlapperParticlesInfo::PROTOCOL_VERSION == ProtocolInfo::CURRENT_PROTOCOL){
+            if (SlapperParticleInfo::PROTOCOL_VERSION == ProtocolInfo::CURRENT_PROTOCOL){
                 $log->info(TextFormat::GREEN."[INFO] Your SlapperParticles is Compatible with your version!");
             } else {
                 $log->info(TextFormat::RED."[ERROR] Your SlapperParticles isnt Compatible with your version!");
                 $this->getServer()->getPluginManager()->disablePlugin($this);
             }
-        if (isset($colorA)) {
+        if (!isset($colorA)) {
             $log->error("[ERROR] Color A is blank!");
             $log->notice("[NOTICE] Color A modified to 255 as default.");
             $this->getConfig()->set("Color A", 255);
             return;
         }
-        if (isset($colorB)) {
+        if (!isset($colorB)) {
             $log->error("[ERROR] Color B is blank!");
             $log->notice("[NOTICE] Color B modified to 255 as default.");
             $this->getConfig()->set("Color B", 255);
             return;
         }
-        if (isset($colorC)) {
+        if (!isset($colorC)) {
             $log->error("[ERROR] Color C is blank!");
             $log->notice("[NOTICE] Color C modified to 255 as default.");
             $this->getConfig()->set("Color C", 255);
@@ -57,7 +57,7 @@ class Main extends PluginBase implements SlapperParticleInfo {
         }
         
         if ($toggle == true){
-            $this->getServer()->getPluginManager()->registerEvents(new SlapperParticleListener($this), 2);
+            $this->getServer()->getPluginManager()->registerEvents(new ParticleEffect($this), 2);
             return;
             }
             
